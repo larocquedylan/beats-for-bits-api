d73fd18be9025e7c2ad680805d3b832427161f57
Dylan LaRocque <dylan@Dylans-MacBook-Pro-2.local>
2023-02-06 18:23:31 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
+// install dependcies
 const express = require('express');
 const app = express();
 const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
+app.use(cors()); // enable CORS - do I need to make this only available for my client?
+app.use(express.json()); // allow api to accept a JSON body via POST / PATCH / ETC
+app.use(express.static("./public" )); // serve pictures for gallery, and songs? -- public isn't in the url
 
-app.use(cors());
-
-const PORT = process.env.port || 1111;
-
-app.get('/wallet', (req, res) => {
-    axios.get(walletEndpoint, process.env)
-        .then(response => {
-        let wallet = response.data; // {name: 'Music', balance: 80000 }
-
-    // write this response to our server or our file system?
-    // writing to file is asynchronous therefore needs a callback
-    // example
-    // fs.writeFile('file-path', JSON.stringify(response.data), () => console.log('file wrote to system)
-
-    // send this response to our front-end api call
-    res.json(wallet); // returns in the response to our client
+const PORT = process.env.PORT || 5050;
 
 
+const walletRoutes = require('./routes/walletRoutes');
+
+app.use("/wallet", walletRoutes);
+
+// app.get("/songs", (req,res) => {
+//     console.log(1);
+//     // get some data from either a json or a database
+
+//     res.json([
+//         {id: 1, title: "song1", filetype: "wav"},
+//         {id: 2, title: "song2", filetype: "wav"}
+//     ])
+// })
+
+app.get("/", (req,res) => {
+    res.send("hello");
 })
-.catch(error => {
-    console.log(error);
-})
-})
-
-const walletEndpoint = "https://legend.lnbits.com/api/v1/wallet";
-const headers = { headers: {'X-api-key': '0dc7444ff5fc446aa53949f96adb7dc1'}};
-
-
-app.get("/songs", (req,res) => {
-    console.log(1);
-    // get some data from either a json or a database
-
-    res.json([
-        {id: 1, title: "song1", filetype: "wav"},
-        {id: 2, title: "song2", filetype: "wav"}
-    ])
-})
-
 
 app.listen(PORT, () => {
-    console.log(` APP listening on port ${PORT} ` );
+    console.log(` App listening on port ${PORT} ` );
 })
